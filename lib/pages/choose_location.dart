ffd58ddd1027f7d6a5b257faf30074aee57beba6
Rajtuladhar <tuladharraj09@gmail.com>
2022-06-30 20:02:04 +0545

    my first commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'United Kingdom', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Greece', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Egypt', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Kenya', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'United State Of America', flag: 'usa.png'),
+    // WorldTime(url: 'America/New_York', location: '', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'South Korea', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Indonesia', flag: 'indonesia.png'),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    updateTime(index);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
